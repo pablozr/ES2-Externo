1e2aac575355532f6a4d25d36a22f5637e847088
pablozr <apollo.zero@outlook.com>
2025-12-01 18:59:41 -0300

    feat: was added cartao validation

--- main.py
+++ main.py
@@ -27,4 +27,6 @@ app.include_router(email_router)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host='0.0.0.0', port=port)
