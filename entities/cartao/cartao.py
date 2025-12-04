d8b8046a298fabc67f0d5f27b104bad4c8a336c6
Pablo <apollo.zero@outlook.com>
2025-12-04 10:46:20 -0300

    fix: was fixed validar cartao

--- entities/cartao/cartao.py
+++ entities/cartao/cartao.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class Cartao(BaseModel):
-    nome_titular: str
+    nomeTitular: str
     numero: str = Field(..., min_length=13, max_length=19)
     validade: str = Field(..., pattern=r"^\d{4}-\d{2}-\d{2}$")
     cvv: str
--- routes/cartao/router.py
+++ routes/cartao/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
 from entities.cartao.cartao import Cartao
-from functions.stripe.stripe_manager import stripe_instance
+from functions.stripe.mercado_pago_manager import mercado_pago_instance
 
 router = APIRouter()
 
@@ -11,7 +11,7 @@ async def valida_cartao(cartao: Cartao):
     cartao = cartao.model_dump()
 
     try:
-        response = await stripe_instance.valida_cartao(cartao)
+        response = await mercado_pago_instance.valida_cartao(cartao)
 
         if not response["status"]:
             return JSONResponse(status_code=400, content={"codigo": 400, "mensagem": response["mensagem"]})
